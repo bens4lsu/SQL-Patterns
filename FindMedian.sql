2908e3aff75db3e09107a21d910e88cabfe27daf
Ben Schultz <47571353+bens4lsu@users.noreply.github.com>
2025-07-23 13:18:27 -0500

    Update FindMedian.sql
    Add code for if you need to unpivot several columns and find the median of that.

--- FindMedian.sql
+++ FindMedian.sql
@@ -8,3 +8,22 @@ SELECT MAX(val) FROM (
 	FROM cteTestData
 	ORDER BY val
 ) Y
+
+
+
+;WITH cteTestData AS (
+	SELECT *
+	FROM (VALUES (1, 19, 12, 8, CAST(NULL as int))) z (valA, valB, valC, valD, valE)
+)
+, cteUnpivoted AS (
+	SELECT val
+	FROM cteTestData
+	UNPIVOT (
+		val FOR columnName IN (valA, valB, valC, valD, valE)
+	) as unpiv
+)
+SELECT MAX(val) FROM (
+	SELECT top 51 PERCENT val 
+	FROM cteUnpivoted
+	ORDER BY val
+) Y
